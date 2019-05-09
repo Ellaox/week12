78ee472c5e01bec41c9c2e1518e9b709f58c6628
Ellaox <40073721+Ellaox@users.noreply.github.com>
2019-05-09 12:24:58 +0100

    Add files via upload

---
+++ dailyPaper2.py
@@ -0,0 +1,17 @@
+# dailyPaper2.py
+# stores a list of newspapers available in the 
+# student shop and displays them on the screen
+# using list indexing
+# D.H. February 2005
+
+
+print "This program will display the newspapers"
+print "available in the student shop.n"
+newspaper = ["Independent", "Guardian", "Times", "Telegraph", "Mirror"]
+# access and display each element of the list
+print newspaper[0],
+print newspaper[1],
+print newspaper[2],
+print newspaper[3],
+print newspaper[4],
+    
