67b814108466928b52e17392233e4d66910abe72
Ellaox <40073721+Ellaox@users.noreply.github.com>
2019-05-09 12:27:05 +0100

    Add files via upload

---
+++ dailyPaper3.py
@@ -0,0 +1,14 @@
+# dailyPaper3.py
+# stores a list of newspapers available in the 
+# student shop and displays them on the screen
+# using list indexing
+# D.H. February 2005
+ 
+
+print "This program will display the newspapers"
+print "available in the student shop.\n"
+newspaper = ["Independent", "Guardian", "Times", "Telegraph", "Mirror"]
+# access and display each element of the list newspaper
+# using the range function and list indexing
+for i in range(5):
+    print newspaper[i],
